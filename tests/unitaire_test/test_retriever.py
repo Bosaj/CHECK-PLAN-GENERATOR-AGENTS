c2cef77529cc149ef2da96d2835ebfba27c447a6
startlingadama <startlingadama@gmail.com>
2025-09-22 07:50:22 +0100

    Test unitaire

---
+++ tests/unitaire_test/test_retriever.py
@@ -0,0 +1,8 @@
+from check_planner.retriever import retrieve_regulation
+import pytest
+import asyncio
+
+def test_retrieve_regulation():
+    result = asyncio.run(retrieve_regulation("Article 30:  la mise en œuvre par la société gest"))
+    assert result
+    assert "Article 30" in result[0][0]
